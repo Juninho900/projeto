79fb5e45007c2bed01c1ae679acfc9e69d421cbb
Antonio <jrrotta@outlook.com>
2021-10-07 15:23:31 -0300

    teste7

--- database/migrations/2021_10_07_142603_create_posts_table.php
+++ database/migrations/2021_10_07_142603_create_posts_table.php
@@ -15,6 +15,19 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 100);
+            $table->string('email', 100);
+            $table->string('telefone', 30);
+            $table->date('dtNasc');
+            $table->boolean('ativo');
+            $table->string('ra',10);
+            $table->string('nomeProf', 100);
+            $table->string('emailProf', 100);
+            $table->string('telefoneProf', 30);
+            $table->string('numReg', 30);
+            $table->string('titulacao', 30);
+            $table->string('tituloAula', 100);
+            $table->string('curso', 50);
             $table->timestamps();
         });
     }
